b18c698c82abbb003a6d1800863b22b268b8d898
notnasobe666 <soeberg.anton@gmail.com>
2020-03-15 09:54:11 +0100

    anton

--- Patient 0.py
+++ Patient 0.py
@@ -8,3 +8,19 @@ x = "hallo"
 print(3+4+5)
 # stupid
 
+
+def optimized_tax_revenue(x):
+    t_0 = x[0]
+    t_1 = x[1]
+    k   = x[2]
+    return -tax_revenue(e,v,m,t0,t1,k,N)
+
+Guess = [0.2,0.3,0.4]
+
+solution = optimize.minimize(optimized_tax_revenue,Guess,method='SLSQP',bounds=((0,1),(0,1),(0,1))))
+t0  = solution.x[0]
+t1  = solution.x[1]
+k   = solution.x[2] 
+T   = tax_revenue(e,v,m,t0,t1,k,N)
+
+print(t0)
--- test.py
+++ test.py
@@ -130,16 +130,20 @@ print('The difference in tax revenue is: '+ str(tax_revenue_e_new-tax_revenue))
 
 # Optimize the tax 
 
-Guess = [0.1,0.1,0.1]
-# Same optimization formula as above
-def tax_optimize(t0,t1,k):
-    tax_opt = optimize.minimize(tax_revenue,Guess,method='SLSQP',bounds=((0,1),(0,1),(0,1)))
-    t0_opt=tax_opt.x
-    return t0_opt
-  
-t0_opt = tax_optimize(t0,t1,k)
+def optimized_tax_revenue(x):
+    t_0 = x[0]
+    t_1 = x[1]
+    k   = x[2]
+    return -tax_revenue(e,v,m,t0,t1,k,N)
 
-print('Optimal t0 is:' + str(t0_opt))
+Guess = [0.2,0.3,0.4]
 
+solution = optimize.minimize(optimized_tax_revenue,Guess,method='SLSQP',bounds=((0,1),(0,1),(0,1))))
+t0  = solution.x[0]
+t1  = solution.x[1]
+k   = solution.x[2] 
+T   = tax_revenue(e,v,m,t0,t1,k,N)
+
+print(t0)
 
 
