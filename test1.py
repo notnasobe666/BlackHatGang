963e7b2f667de97abcd8d920d8d2696cb404af99
notnasobe666 <soeberg.anton@gmail.com>
2020-03-15 16:24:15 +0100

    anton

--- test1.py
+++ test1.py
@@ -18,5 +18,37 @@ e   = 0.3
 t0  = 0.4
 t1  = 0.1
 k   = 0.4
-w   = 0.5
+
+# Now the model equations are defined
+
+def x(m,w,l,t0,t1,k): 
+    """x equals the optimal consumption"""
+
+    return m + w*l - (t0*w*l + t1*np.max(w*l-k,0))
+
+def utility(l,c,e,v):
+    return np.log(c) - v * ((l** (1+(1/e)))/(1+ (1/e)))
+
+def labor_supply(l,e,v,m,w,t0,t1,k):
+    c = x(m,w,l,t0,t1,k)
+    return -utility(l,c,e,v)
+
+def supply_problem(e,v,m,w,t0,t1,k):
+    solution =  optimize.minimize_scalar(labor_supply,method='bounded', \
+                bounds=(0,1), args=(e,v,m,w,t0,t1,k))
+    l = solution.x
+    c = x(m,w,l,t0,t1,k)
+    u = utility(l,c,e,v)
+    return l,c,u
+
+l_star = supply_problem(e,v,m,w,t0,t1,k)[0]
+c_star = supply_problem(e,v,m,w,t0,t1,k)[1]
+u_star = supply_problem(e,v,m,w,t0,t1,k)[2]
+
+print('Optimal labour supply is: ' + str(l_star))
+print(c_star)
+print(u_star)
+
+# Question 2
+
 
