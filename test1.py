1e7e60644280443e63433834c47b38ab245dac70
notnasobe666 <soeberg.anton@gmail.com>
2020-03-15 17:29:47 +0100

    anton

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from scipy import optimize 
 import matplotlib.pyplot as plt 
-import math as math
+import math
 
 # autoreload modules when code is run
 %load_ext autoreload
@@ -24,10 +24,10 @@ k   = 0.4
 def x(m,w,l,t0,t1,k): 
     """x equals the optimal consumption"""
 
-    return m + w*l - (t0*w*l + t1*np.max(w*l-k,0))
+    return m + w*l - (t0*w*l + t1*max(w*l-k,0))
 
 def utility(l,c,e,v):
-    return np.log(c) - v * ((l** (1+(1/e)))/(1+ (1/e)))
+    return math.log(c) - v * ((l** (1+(1/e)))/(1+ (1/e)))
 
 def labor_supply(l,e,v,m,w,t0,t1,k):
     c = x(m,w,l,t0,t1,k)
@@ -53,7 +53,7 @@ print(u_star)
 
 # A vector for w is created
 N       = 10000
-w_vec   = np.random.uniform(0.5, 1.5, size=N)
+w_vec   = np.random.uniform(0.5, 1.5, size=100)
 
 # empty lists for l and c is created
 c_vec = np.empty(N)
@@ -82,13 +82,13 @@ def total_tax_revenue(e,v,m,t0,t1,k,N):
     total_tax_revenue = 0 
     np.random.seed(666)
     for i in range(1,N+1):
-        w_i                 = np.random.uniform(0.5, 1.5)
+        w_i                 = np.random.uniform(low=0.5, high=1.5)
         l_i, _, _           = supply_problem(e,v,m,w_i,t0,t1,k)
-        total_tax_revenue   += (t0*w_i*l_i + t1*np.max(w_i*l_i-k,0))
+        total_tax_revenue   += (t0*w_i*l_i + t1*max(w_i * l_i - k,0))
         return total_tax_revenue
 
 # Calculate the total tax revenue
 
-T = total_tax_revenue(e,v,m,t0,t1,k,N)
+T = total_tax_revenue(e, v, m, t0, t1, k, N)
 print(T)
 
