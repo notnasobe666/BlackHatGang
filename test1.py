5b8aa91f1087473c997e0a0eea9dc1953b930c6c
notnasobe666 <soeberg.anton@gmail.com>
2020-03-15 14:51:53 +0100

    anton

--- test1.py
+++ test1.py
@@ -13,7 +13,16 @@ import math as math
 
 # first the objective is defined
 def objective(x):
-    c = 
-    l = 
-    v = 
-    e =
+    c = x[0]
+    l = x[1]
+    v = x[2]
+    e = x[3]
+    return np.log(c) - v * (l**(1+(1/e)))/(1+(1/e))
+
+# then the constraint(s) are defined
+
+def constraint1(x):
+    return m + w*l - (t0*w*l+t1*np.max(w*l-k,0)) - z
+
+x0 = [1,1,1,1]
+print(objective(x0))
