0fcb5743d5e2664eb5f90f3ad0c6fbf7fa7af44b
notnasobe666 <soeberg.anton@gmail.com>
2020-03-15 14:38:44 +0100

    anton

---
+++ test1.py
@@ -0,0 +1,19 @@
+import numpy as np 
+from scipy import optimize 
+import matplotlib.pyplot as plt 
+import math as math
+
+# autoreload modules when code is run
+%load_ext autoreload
+%autoreload 2
+
+# Question 1
+
+# Construct a function which solves eq. (1)
+
+# first the objective is defined
+def objective(x):
+    c = 
+    l = 
+    v = 
+    e =
