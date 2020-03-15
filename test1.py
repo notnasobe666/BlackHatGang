39663da24b7fc905bd911adf31613107c2a106f8
notnasobe666 <soeberg.anton@gmail.com>
2020-03-15 15:48:45 +0100

    anton

--- test1.py
+++ test1.py
@@ -11,18 +11,12 @@ import math as math
 
 # Construct a function which solves eq. (1)
 
-# first the objective is defined
-def objective(x):
-    c = x[0]
-    l = x[1]
-    v = x[2]
-    e = x[3]
-    return np.log(c) - v * (l**(1+(1/e)))/(1+(1/e))
+# Following variables is known
+m   = 1
+v   = 10
+e   = 0.3
+t0  = 0.4
+t1  = 0.1
+k   = 0.4
+w   = 0.5
 
-# then the constraint(s) are defined
-
-def constraint1(x):
-    return m + w*l - (t0*w*l+t1*np.max(w*l-k,0)) - z
-
-x0 = [1,1,1,1]
-print(objective(x0))
