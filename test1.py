cc25660023fd652a5c7bd0a4e96fb1998bc407e9
Asgerz <fhc614@alumni.ku.dk>
2020-03-16 13:35:13 +0100

    AZ

--- test1.py
+++ test1.py
@@ -46,9 +46,9 @@ l_star = supply_problem(e,v,m,w,t0,t1,k)[0]
 c_star = supply_problem(e,v,m,w,t0,t1,k)[1]
 u_star = supply_problem(e,v,m,w,t0,t1,k)[2]
 
-print('Optimal labour supply is: ' + str(l_star))
-print(c_star)
-print(u_star)
+print(f'Optimized labour supply is: {l_star:.2f}')
+print(f'Optimized consumption is:  {c_star:.2f}')
+print(f'Optimal utility is: {u_star:.2f}')
 
 # Question 2
 
@@ -79,49 +79,80 @@ plt.show()
 # A function to return the total tax revenue is created to be \
 # used in following questions
 
-def total_tax_revenue(e,v,m,t0,t1,k,N):
-    total_tax_revenue = 0 
-    np.random.seed(666)
-    for i in range(1,N+1):
-        w_i                 = np.random.uniform(low=0.5, high=1.5)
-        l_i, _, _           = supply_problem(e,v,m,w_i,t0,t1,k)
-        total_tax_revenue   += (t0*w_i*l_i + t1*max(w_i * l_i - k,0))
-    return total_tax_revenue
+np.random.seed(4600)
+PopLarge = np.random.uniform(0.5,1.5,size=10000)
 
-# Calculate the total tax revenue
+def TotalTax(PopVec,e,v,t0,t1,k,m):
 
-T = total_tax_revenue(e, v, m, t0, t1, k, N)
-print(T)
+    N=len(PopVec)
+    PopTaxes=np.zeros((N))
+
+    for i,w in enumerate(PopVec):
+        #3.3.1. Return optimal labour supply given wage from the optimiser function
+        Ind_optimum=optimiser(w,e,v,t0,t1,k,m)
+        IndLabour=Ind_optimum[0]
+        #3.3.2. Return tax payment given the income yielded by optimal labour supply
+        PopTaxes[i]=t0*w*IndLabour+t1*max(w*IndLabour-k,0)
+    #3.4. Sum over all tax payments
+    TotTax=sum(PopTaxes)
+    return TotTax
+
+#3.5. Call total tax functions given the array of randomly drawn wages.
+TotTax0 = TotalTax(PopLarge,e,v,t0,t1,k,m)
+print(f'The total tax revenue is {TotTax0:.1f}')
+
 
 # Question 4
 # Frisch Elasticity changed from 0.3 to 0.1
 e_new = 0.1
 
 T_new = total_tax_revenue(e_new,v,m,t0,t1,k,N)
-print(T_new)
+print(f'New total tax revenue: {T_new:.2f}')
 
 
 # Question 5
 
 # Define the negative of total tax revenue
-N_test = 100
-def optimized_tax_revenue(x):
-    t0  = x[0]
-    t1  = x[1]
-    k   = x[2]
-    return -total_tax_revenue(e,v,m,t0,t1,k,N_test)
+#5.1 Draw alternative vector 
+PopWageS = np.random.uniform(0.5,1.5,size=100)
+PopWageM = np.random.uniform(0.5,1.5,size=1000)
 
 
-guess   = [0.1,0.1,0.1]
-tax_solution = optimize.minimize(optimized_tax_revenue,guess,\
-    method='SLSQP',bounds=((0,1),(0,1),(0,1)))
+#5.2.1  Create a vector 'taxes' which includes 'ltax', 'ttax' and 'cutoff'
+        #return the total tax for the 100, 1000 or 10000 people
+def value_of_choice_tax(taxes,PopWage,w,v,m):
+    
+    t0 = taxes[0]
+    t1 = taxes[1]
+    k = taxes[2]
+    return -total_tax_revenue(e,v,m,t0,t1,k,N)
 
-t0      = tax_solution.x[0]
-t1      = tax_solution.x[1]
-k       = tax_solution.x[2]
-T_opt   = total_tax_revenue(e,v,m,t0,t1,k,N_test)
+#5.2.2  Define the 'taxOptimiser' function, find the tax maximising values of the vector 'taxes'.
+def taxOptimiser(PopWage,e,v,m):
+    
+    # 5.2.3 Call solver
+    initial_guess = [0.785,0.055,0.470]
+    sol_case3 = optimize.minimize(
+        value_of_choice_tax,initial_guess,method='Nelder-Mead',
+        args=(PopWage,e,v,m))
 
-print(t0)
-print(t1)
-print(k)
-print(T_opt)
+    t0Star=sol_case3.x[0]
+    t1Star=sol_case3.x[1]
+    kStar=sol_case3.x[2]
+
+    #5.2.4 Print the solution   
+    print(f'Optimal lower tax rate is {t0Star:.3f}')
+    print(f'Optimal upper tax rate is {t1Star:.3f}')
+    print(f'Optimal cutoff income is {kStar:.3f}')
+    
+    return[t0Star,t1Star,kStar]
+
+print('Optimal taxes and estimated total tax revenue, N=100')
+[t0Star,t1Star,kStar]=taxOptimiser(PopWageSmall,e,v,m)
+TotTaxSmall = TotalTax(PopWageLarge,e,v,t0Star,t1Star,kStar,m)
+print(f'Estimated total tax revenue is {TotTaxSmall:.1f}')
+
+print('Optimal taxes and estimated total tax revenue, N=1000')
+[t0Star,t1Star,kStar]=taxOptimiser(PopWageMedium,e,v,money)
+TotTaxMedium = TotalTax(PopWageLarge,e,v,t0Star,t1Star,kStar,m)
+print(f'Estimated total tax revenue is {TotTaxMedium:.1f}')
