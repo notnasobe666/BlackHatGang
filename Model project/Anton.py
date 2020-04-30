fc7e6877bd638da98a557e5ba4d4fc2428e22a1a
notnasobe666 <soeberg.anton@gmail.com>
2020-04-30 12:27:40 +0200

    anton

--- Model project/Anton.py
+++ Model project/Anton.py
@@ -15,8 +15,15 @@ import scipy.interpolate as sci
 from pandas_datareader import data as pdr
 import yfinance as yf
 
+# msft = yf.Ticker("msft")
+# hist = msft.history(start=start_date, end=end_date)
+# print(hist["Close"])
 
-msft = yf.Ticker("MSFT")
-hist = msft.history(period="5d")
-print(hist["Close"])
+start_date = datetime.datetime(2010,1,1)
+end_date = datetime.datetime(2020,1,1)
 
+yf.pdr_override() # <== that's all it takes :-)
+
+# download dataframe
+data = pdr.get_data_yahoo(["SPY","AAPL"], start=start_date, end=end_date)
+data["Adj Close"].head(5)
