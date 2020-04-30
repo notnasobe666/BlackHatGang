e929f48f15403d4fc9889629b90f06ebebe02ed4
notnasobe666 <soeberg.anton@gmail.com>
2020-04-30 12:04:05 +0200

    anton

--- Model project/Anton.py
+++ Model project/Anton.py
@@ -2,8 +2,19 @@
 
 ## install pip yfinance
 
-
+## packages and imports
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+import scipy.stats as scs
+import scipy.optimize as sco
+import statsmodels.api as sm
+import scipy.interpolate as sci
+from pandas_datareader import data as pdr
 import yfinance as yf
+
 
 msft = yf.Ticker("MSFT")
 hist = msft.history(period="5d")
