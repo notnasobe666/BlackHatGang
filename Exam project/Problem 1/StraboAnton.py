9aabd490205663525dcf0fe70b4a705659b16e25
notnasobe666 <soeberg.anton@gmail.com>
2020-05-16 12:12:07 +0200

    fsa

--- Exam project/Problem 1/StraboAnton.py
+++ Exam project/Problem 1/StraboAnton.py
@@ -57,8 +57,34 @@ X_trans_y = X_trans.dot(y)
 X_trans_y
 
 beta_hat = Inv_X_X_trans.dot(X_trans_y)
+beta_hat
+
 betas = pd.DataFrame({'Beta 0':beta_hat,'Beta 1':beta_hat,'Beta 2':beta_hat})
 betas
 
 #######################################################################################
+
+# Question 2
+
+# Lav stor fed 3D plot med beta vægte ganget på mulige forekomster af x1 og x2. 
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+y_hat = np.dot(X,beta_hat)
+y_hat
+df_y_hat = pd.DataFrame({'x1':x1,'x2':x2,'y_hat':y_hat})
+df_y_hat.head()
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(df_y_hat['x1'],df_y_hat['x2'],df_y_hat['y_hat'], c='pink',marker='o',alpha=1)
+ax.set_xlabel('x1')
+ax.set_ylabel('x2')
+ax.set_zlabel('y3')
+plt.show()
+
+#######################################################################################
+
+# Question 3
 
