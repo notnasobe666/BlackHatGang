66c332a35a90f57adc9664b93650d9c8b9c982c0
notnasobe666 <soeberg.anton@gmail.com>
2020-05-16 11:04:25 +0200

    as

--- Exam project/Problem 1/StraboAnton.py
+++ Exam project/Problem 1/StraboAnton.py
@@ -2,4 +2,50 @@
 
 # packagaes 
 import numpy as np 
+import pandas as pd
 
+# y_i = beta0 + beta1 x1_i + beta2 x2_i + epsilon
+
+# data generating proces - inserted from exam description
+def DGP(N):
+    
+    # a. independent variables
+    x1 = np.random.normal(0,1,size=N)
+    x2 = np.random.normal(0,1,size=N)
+    
+    # b. errors
+    eps = np.random.normal(0,1,size=N)
+    
+    extreme = np.random.uniform(0,1,size=N)
+    eps[extreme < 0.05] += np.random.normal(-5,1,size=N)[extreme < 0.05]
+    eps[extreme > 0.95] += np.random.normal(5,1,size=N)[extreme > 0.95]
+    
+    # c. dependent variable
+    y = 0.1 + 0.3*x1 + 0.5*x2 + eps
+    
+    return x1, x2, y
+
+# accessable data - inserted from exam description
+
+np.random.seed(2020)
+x1,x2,y = DGP(10000)
+
+############################################################################
+
+# To define X, x1 and x2 must be included in the same dataframe. Also a column of one's 
+# for Beta_0.
+
+x0 = 1
+X = pd.DataFrame({'x0':x0,'x1':x1,'x2':x2})
+X
+
+# Question 1 
+
+# Now X can be transposed etc by numpy formulas. 
+
+X_trans = X.T
+X_trans
+Inv_X_X_trans = 
+y = 
+X_trans_y = 
+beta_hat = 
